f4572c129f053a58a632991e137a03d13a1d06cd
DjAdria001 <adria30042004@gmail.com>
2025-02-17 13:51:42 +0100

    1

---
+++ TA4/src/T04_4.java
@@ -0,0 +1,15 @@
+
+public class T04_4 {
+
+	public static void main(String[] args) {
+		int N=20, result=0;
+		System.out.println("Valor inical de N = " + N);
+		result = N + 70;
+		System.out.println("N + 77 = " + result);
+		N = result - 3;
+		System.out.println("N - 3 = " + N);
+		result = N * 2;
+		System.out.println("N * 2 = " + result);
+	}
+
+}
---
+++ TA4/src/T04_5.java
@@ -0,0 +1,16 @@
+
+public class T04_5 {
+
+	public static void main(String[] args) {
+		int A=5, B=10, C=15, D=20;
+		B=C;
+		C=A;
+		A=D;
+		D=B;
+		System.out.println("A es: "+ A);
+		System.out.println("B es: "+ B);
+		System.out.println("C es: "+ C);
+		System.out.println("D es: "+ D);
+	}
+
+}
