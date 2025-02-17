94af2f868278c6d5eca9a4dccad59abc61d05a73
DjAdria001 <adria30042004@gmail.com>
2025-02-17 12:22:25 +0100

    1

--- TA4/src/T04_2.java
+++ TA4/src/T04_2.java
@@ -4,11 +4,15 @@ public class T04_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int N=30;
-		double A=25.5;
-		char C=97;
+		double A=25.5, result=0;
+		char C=107;
+		
 		System.out.println("El valor de N es " + N);
 		System.out.println("El valor de A es " + A);
 		System.out.println("El valor de C es " + C);
+		System.out.println("La Suma de N y A es " + (N + A));
+		System.out.println("La Resta de N y A es " + (N - A));
+		System.out.println("El Valor Numerico del Caracter K es "+(C + result));
 	}
 
 }
---
+++ TA4/src/T04_3.java
@@ -0,0 +1,9 @@
+
+public class T04_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
