87861e16afd89f387f98643523247939f2846c58
DjAdria001 <adria30042004@gmail.com>
2025-02-17 12:04:04 +0100

    1

---
+++ TA4/src/T04_1.java
@@ -0,0 +1,19 @@
+
+public class T04_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=30, b=25, result=0;
+		result = a + b;
+		System.out.println("La suma de a(30) y b(25) es " + result);
+		result = a - b;
+		System.out.println("La resta de a(30) y b(25) es " + result);
+		result = a * b;
+		System.out.println("La multiplicacion de a(30) y b(25) es " + result);
+		result = a / b;
+		System.out.println("La division de a(30) y b(25) es " + result);
+		result = a % b;
+		System.out.println("El resto de a(30) y b(25) es " + result);
+	}
+
+}
---
+++ TA4/src/T04_2.java
@@ -0,0 +1,14 @@
+
+public class T04_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int N=30;
+		double A=25.5;
+		char C=97;
+		System.out.println("El valor de N es " + N);
+		System.out.println("El valor de A es " + A);
+		System.out.println("El valor de C es " + C);
+	}
+
+}
