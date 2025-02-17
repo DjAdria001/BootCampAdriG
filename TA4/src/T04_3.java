7e874e97ce51cdc34b389b5fb65d4cd2c7e66118
DjAdria001 <adria30042004@gmail.com>
2025-02-17 12:46:35 +0100

    Update T04_3.java

--- TA4/src/T04_3.java
+++ TA4/src/T04_3.java
@@ -2,7 +2,31 @@
 public class T04_3 {
 
 	public static void main(String[] args) {
-
+		int X=10, Y=11;
+		double N=12.25, M=13.5;
+		System.out.println("El Valor de X es "+ X);
+		System.out.println("El Valor de Y es "+ Y);
+		System.out.println("El Valor de N es "+ N);
+		System.out.println("El Valor de M es "+ M);
+		System.out.println("La Suma de X e Y es " + (X + Y));
+		System.out.println("La Diferencia de X e Y es " + (X - Y));
+		System.out.println("El Producto de X e Y es " + (X * Y));
+		System.out.println("El Cociente de X e Y es " + (X / Y));
+		System.out.println("La Diferencia de X e Y es " + (X % Y));
+		System.out.println("La Suma de N y M es " + (N + M));
+		System.out.println("La Resta de N y M es " + (N - M));
+		System.out.println("El Producto de N y M es " + (N * M));
+		System.out.println("El Cociente de N y M es " + (N / M));
+		System.out.println("La Diferencia de N y M es " + (N % M));
+		System.out.println("La Suma de X y N es " + (X + N));
+		System.out.println("El Cociente de Y y M es " + (Y / M));
+		System.out.println("El resto de Y y M es " + (Y % M));
+		System.out.println("El Doble de X es " +(X * 2));
+		System.out.println("El Doble de Y es " +(Y * 2));
+		System.out.println("El Doble de N es " +(N * 2));
+		System.out.println("El Doble de M es " +(M * 2));
+		System.out.println("La Suma de todas las Variavles da "+(X + Y + N + M));
+		System.out.println("El Producto de todas las Variables da "+(X * Y * N * M));
 	}
 
 }
