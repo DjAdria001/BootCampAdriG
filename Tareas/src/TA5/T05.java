89c0167db53e4e44b4a9410e6634087ec657ab8e
DjAdria001 <adria30042004@gmail.com>
2025-02-18 12:37:32 +0100

    Update T05.java

--- Tareas/src/TA5/T05.java
+++ Tareas/src/TA5/T05.java
@@ -4,12 +4,14 @@ public class T05 {
 
 	public static void main(String[] args) {
 
-		int A=22, B=28;
+		int A=33, B=28;
 		boolean resultado;
-		resultado = A<=B;
-		if(resultado=false)System.out.println("A es mayor que B");
-		if(resultado=true)System.out.println("B es mayor que A");
+		resultado = A<B;
 		System.out.println(resultado);
+		if(resultado=true)System.out.println("B es mayor que A");
+		if(resultado=false)System.out.println("A es mayor que B");
+	
+
 	}
 
 }
