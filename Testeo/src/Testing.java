4ae0a1dfd0243009fdf70504393c3a204a778ba0
DjAdria001 <adria30042004@gmail.com>
2025-02-21 10:17:41 +0100

    Update Testing.java

--- Testeo/src/Testing.java
+++ Testeo/src/Testing.java
@@ -5,7 +5,6 @@ public class Testing {
 	public static void main(String[] args) {
 
 		int A=20, B=20;
-		boolean resultado=false;
 		
 		if (A<B) {System.out.println(B +" es mayor que "+ A);}
 		else if (B<A) {System.out.println(A +" es mayor que "+ B);}
