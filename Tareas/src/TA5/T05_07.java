47c2cfa4f58f3da8f99143445833f70dca25171e
DjAdria001 <adria30042004@gmail.com>
2025-02-21 13:52:49 +0100

    1

---
+++ Tareas/src/TA5/T05_07.java
@@ -0,0 +1,15 @@
+package TA5;
+
+public class T05_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=1;
+		while (num<=100) {
+			System.out.println(num);
+			num++;
+		};
+			
+		}
+}
+
