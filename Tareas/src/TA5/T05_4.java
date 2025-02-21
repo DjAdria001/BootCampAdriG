4249773d6032acbe7b658a1350b5364c20391d1b
DjAdria001 <adria30042004@gmail.com>
2025-02-21 12:04:49 +0100

    Update T05_4.java

--- Tareas/src/TA5/T05_4.java
+++ Tareas/src/TA5/T05_4.java
@@ -7,10 +7,9 @@ public class T05_4 {
 	public static void main(String[] args) {
 		final double pi = Math.PI;
 		String texto_num = JOptionPane.showInputDialog("Introduce el Radio del Circulo");
-		double Radio = Integer.parseInt(texto_num);
-
-		double radio2 = Math.pow(Radio, 2);
-		JOptionPane.showMessageDialog(null, "El Radio es de " + Radio + ". Y su area total es de " + (pi * (radio2)));
+		double Radio = Double.parseDouble(texto_num);
+		
+		JOptionPane.showMessageDialog(null, "El Radio es de " + Radio + ". Y su area total es de " + (pi * (Math.pow(Radio, 2))));
 
 	}
 
