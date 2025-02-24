3f4a1f31fb30693cb2c09a5e6b5cc7439c022d2b
DjAdria001 <adria30042004@gmail.com>
2025-02-24 10:54:52 +0100

    Update T04_5.java

--- Tareas/src/TA4/T04_5.java
+++ Tareas/src/TA4/T04_5.java
@@ -4,10 +4,11 @@ public class T04_5 {
 
 	public static void main(String[] args) {
 		int A=5, B=10, C=15, D=20;
+		int B1=B; //Esta b guarda el valor para prevenir que el resultado de D=B no sea D=C
 		B=C;
 		C=A;
 		A=D;
-		D=B;
+		D=B1;
 		System.out.println("A es: "+ A);
 		System.out.println("B es: "+ B);
 		System.out.println("C es: "+ C);
