382cd373ab51d55dc6ce5fe80b7935c54d6150a9
DjAdria001 <adria30042004@gmail.com>
2025-02-24 12:28:46 +0100

    Update T05_10.java

--- Tareas/src/TA5/T05_10.java
+++ Tareas/src/TA5/T05_10.java
@@ -4,10 +4,18 @@ public class T05_10 {
 
 	public static void main(String[] args) {
 		String NumVen = JOptionPane.showInputDialog("Introduce el Numero de ventas:");
-		String V = JOptionPane.showInputDialog("");
-//		Mostramos las variables recibidas
-		System.out.println("Numero de ventas: " + NumVen);
-		System.out.println("Ventas: " + V);
+
+		int NumVentas = Integer.parseInt(NumVen);
+		int i = 0;
+		int TotalVentas = 0;
+		while (i < NumVentas) {
+			String Venta = JOptionPane.showInputDialog("Introduce el valor de la venta:");
+			int VentaInt = Integer.parseInt(Venta);
+			TotalVentas = TotalVentas + VentaInt;
+			i++;
+		}
+		JOptionPane.showMessageDialog(null, "El total de ventas es: " + TotalVentas);
+		
 	}
 
 }
