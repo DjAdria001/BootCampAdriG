0ab17813e9a8b3ee531112fd10718360659f64c5
DjAdria001 <adria30042004@gmail.com>
2025-02-26 12:51:06 +0100

    Update TA5_EXT_5.java

--- Tareas/src/UD5_TA_EXT/TA5_EXT_5.java
+++ Tareas/src/UD5_TA_EXT/TA5_EXT_5.java
@@ -1,10 +1,24 @@
 package UD5_TA_EXT;
 
+import javax.swing.JOptionPane;
+
 public class TA5_EXT_5 {
 
 	public static void main(String[] args) {
-		
-
+		double NumRandom1 = Math.random();
+		long NumRandom2 = Math.round(NumRandom1 * 100);
+//		System.out.println(NumRandom2);
+		String Pregunta = JOptionPane.showInputDialog("Adivina el numero: ");
+		int PregNum = Integer.parseInt(Pregunta);
+		while (PregNum != NumRandom2) {
+			if (PregNum < NumRandom2) {
+				JOptionPane.showMessageDialog(null, "El numero es mayor");
+			} else if (PregNum > NumRandom2) {
+				JOptionPane.showMessageDialog(null, "El numero es menor");
+			}
+			Pregunta = JOptionPane.showInputDialog("Vuelve a Intentarlo: ");
+			PregNum = Integer.parseInt(Pregunta);
+		}
+		JOptionPane.showMessageDialog(null, "Has acertado");
 	}
-
 }
