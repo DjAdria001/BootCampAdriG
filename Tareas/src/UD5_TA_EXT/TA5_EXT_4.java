8b6a980b84757b49e31a093f57d656b63ff9f5a5
DjAdria001 <adria30042004@gmail.com>
2025-02-26 13:51:16 +0100

    1

---
+++ Tareas/src/UD5_TA_EXT/TA5_EXT_4.java
@@ -0,0 +1,22 @@
+package UD5_TA_EXT;
+
+import javax.swing.JOptionPane;
+
+public class TA5_EXT_4 {
+	public static void main(String[] args) {
+		String Palabra = JOptionPane.showInputDialog("Introduce una palabra: ");
+		Palabra = Palabra.toLowerCase();
+		int Vocales = 0;
+		for (int i = 0; i < Palabra.length(); i++) {
+			char letra = Palabra.charAt(i);
+
+			if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
+				Vocales++;
+			}
+
+		}
+		JOptionPane.showMessageDialog(null, "La palabra " + Palabra + " tiene " + Vocales + " vocales.");
+
+	}
+
+}
--- Testeo/src/Testing.java
+++ Testeo/src/Testing.java
@@ -3,6 +3,8 @@ import javax.swing.JOptionPane;
 public class Testing {
 
 	public static void main(String[] args) {
-		
+//		String Pregunta = JOptionPane.showInputDialog(" ");
+//		int PregNum = Integer.parseInt(Pregunta);
+//		javax.swing.JOptionPane.showMessageDialog(null,"");
 	}
 }
