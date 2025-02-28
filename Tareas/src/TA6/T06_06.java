c6b9c12c533b15fc7a98c208ab863860ad132a85
DjAdria001 <adria30042004@gmail.com>
2025-02-28 13:45:36 +0100

    Create T06_06.java

---
+++ Tareas/src/TA6/T06_06.java
@@ -0,0 +1,27 @@
+package TA6;
+
+import javax.swing.JOptionPane;
+
+public class T06_06 {
+
+	public static void main(String[] args) {
+		String Pregunta = JOptionPane.showInputDialog("Introduce un numero entero positivo: ");
+		int PregNum = Integer.parseInt(Pregunta);
+		if (PregNum < 0) {
+			JOptionPane.showMessageDialog(null, "El numero debe ser positivo");
+		} else {
+			JOptionPane.showMessageDialog(null, "El numero " + PregNum + " tiene " + ContarCifras(PregNum) + " cifras");
+		}
+
+	}
+
+	public static int ContarCifras(int num) {
+		int cifras = 0;
+		while (num > 0) {
+			num = num / 10;
+			cifras++;
+		}
+		return cifras;
+	}
+
+}
