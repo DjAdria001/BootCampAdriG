0b837d577103c7848ff12c40773a4bf0d20721a3
DjAdria001 <adria30042004@gmail.com>
2025-03-04 12:25:32 +0100

    Update Testing.java

--- Testeo/src/Testing.java
+++ Testeo/src/Testing.java
@@ -6,5 +6,12 @@ public class Testing {
 //		String Pregunta = JOptionPane.showInputDialog(" ");
 //		int PregNum = Integer.parseInt(Pregunta);
 //		javax.swing.JOptionPane.showMessageDialog(null,"");
+		int num[]= new int[10];
+		num[0]=5;
+		num[1]=9;
+		num[2]=10;
+		System.out.println(num[0]);
+		System.out.println(num[1]);
+		System.out.println(num[2]);
 	}
 }
