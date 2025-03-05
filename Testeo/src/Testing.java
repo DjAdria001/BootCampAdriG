36083cb8e35d10610d3fa2359a84c04c0f280d56
DjAdria001 <adria30042004@gmail.com>
2025-03-05 10:06:46 +0100

    Update Testing.java

--- Testeo/src/Testing.java
+++ Testeo/src/Testing.java
@@ -3,16 +3,11 @@ import javax.swing.JOptionPane;
 public class Testing {
 
 	public static void main(String[] args) {
+
+	}
+
+}
+
 //		String Pregunta = JOptionPane.showInputDialog(" ");
 //		int PregNum = Integer.parseInt(Pregunta);
 //		javax.swing.JOptionPane.showMessageDialog(null,"");
-		
-		int RandomNum = (int) (Math.random() * 10);
-		for (int Div = 2; Div <= (RandomNum - 1); Div++) {
-			if(RandomNum % Div == 0 || RandomNum == 1) {
-				
-			}
-//			System.out.println("numero primo -->" + RandomNum);
-		}
-		}
-}
