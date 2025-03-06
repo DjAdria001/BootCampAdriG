ab6f6127edb7438d83440fe11628e8305e72bbfd
DjAdria001 <adria30042004@gmail.com>
2025-03-06 12:22:31 +0100

    Update T06_11.java

--- Tareas/src/TA6/T06_11.java
+++ Tareas/src/TA6/T06_11.java
@@ -11,7 +11,29 @@ public class T06_11 {
 		int array[][] = new int[Integer.parseInt(Filas)][Integer.parseInt(Columnas)];
 
 		int array2[][] = new int[Integer.parseInt(Filas)][Integer.parseInt(Columnas)];
-
+		int i = 0;
+		while (i < Integer.parseInt(Filas)) {
+			int j = 0;
+			while (j < Integer.parseInt(Columnas)) {
+				array[i][j] = ValorRand();
+				j++;
+			}
+			i++;
+		}
+		Mostrar(array, array.length, array[0].length);
+		
 	}
-
+	public static int ValorRand() {
+		int RandomNum = (int) (Math.random() * 10);
+		return RandomNum;
+	}
+	public static int Mostrar(int[][] num, int Longitud, int Longitud2) {
+		for ( int i = 0; i < Longitud; i++) {
+			for (int j = 0; j < Longitud2; j++) {
+			System.out.println("El valor de num[" + i + "]["+ j+"] es " + num[i][j]);
+			}
+		}
+		return 0;
+	}
 }
+
