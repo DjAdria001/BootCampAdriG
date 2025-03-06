164da1f98099e31895bdc442c7e29146946ebef9
DjAdria001 <adria30042004@gmail.com>
2025-03-06 12:47:30 +0100

    Update T06_11.java

--- Tareas/src/TA6/T06_11.java
+++ Tareas/src/TA6/T06_11.java
@@ -20,7 +20,13 @@ public class T06_11 {
 			}
 			i++;
 		}
+		array2 = array;
+		System.out.println("Array 1");
 		Mostrar(array, array.length, array[0].length);
+		System.out.println("Array 2");
+		Mostrar(array2, array2.length, array2[0].length);
+        int [][] array3 = new int[Integer.parseInt(Filas)][Integer.parseInt(Columnas)];
+		System.out.println("Array 3");
 		
 	}
 	public static int ValorRand() {
@@ -31,9 +37,21 @@ public class T06_11 {
 		for ( int i = 0; i < Longitud; i++) {
 			for (int j = 0; j < Longitud2; j++) {
 			System.out.println("El valor de num[" + i + "]["+ j+"] es " + num[i][j]);
+//			System.out.println("El valor de num[" + i + "] es " + num[i][j]);
 			}
 		}
 		return 0;
 	}
+	public static int [][] multipliArrays(){
+
+        for (int i = 0; i < array.length; i++) {
+        	for (int j = 0; j < Longitud2; j++) {
+            array3[i][j] = array[i][j] * array2[i][j];
+        }
+        }
+        System.out.println("El array multiplicado es: " + array3);
+        return array3;
+
+    }
 }
 
