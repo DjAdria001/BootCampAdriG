f78184be58cf8e9e4b9d16d7265550354e2f10c8
DjAdria001 <adria30042004@gmail.com>
2025-03-10 09:36:20 +0100

    Update T07_01.java

--- Tareas/src/TA7/T07_01.java
+++ Tareas/src/TA7/T07_01.java
@@ -3,9 +3,7 @@ package TA7;
 public class T07_01 {
 
 	public static void main(String[] args) {
-		while(int i=0) {}
-		int Notas[]= {};//Insertar valores en {} por teclado
-		String Alumnos[]= {};//Insertar valores en {} por teclado
+		
 		
 	}
 
