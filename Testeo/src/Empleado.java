639aec8cb35396cd5f0d2e3f33c43040a0a62c3d
DjAdria001 <adria30042004@gmail.com>
2025-03-10 13:43:48 +0100

    Create Empleado.java

---
+++ Testeo/src/Empleado.java
@@ -0,0 +1,19 @@
+
+public class Empleado {
+	private String nombre;
+	
+	private String apellido;
+	
+	private int edad;
+	
+	private double salario;
+	
+	public boolean plus (double sueldoPlus) {
+			boolean aumento=false;
+				if(edad>40) {
+					salario+=sueldoPlus;
+					aumento=true;
+				}
+				return aumento;
+	}
+}
