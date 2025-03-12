8050f1e9c9960dd4664d9a83755ece8cb45ad35f
DjAdria001 <adria30042004@gmail.com>
2025-03-12 09:58:52 +0100

    1

--- Tareas/src/TA7/T07_01.java
+++ Tareas/src/TA7/T07_01.java
@@ -1,10 +1,19 @@
 package TA7;
 
+import java.util.ArrayList;
+
 public class T07_01 {
-
 	public static void main(String[] args) {
-		
-		
+		ArrayList<String> lista=new ArrayList<>();
+		lista.add("a");
+		lista.add("b");
+		lista.add("c");
+		lista.add("d");
+		lista.add("e");
+//		lista.size();
+		for(Object o:lista) {
+			System.out.print(o+" ");
+			
+		}System.out.println(lista.size());
 	}
-
 }
