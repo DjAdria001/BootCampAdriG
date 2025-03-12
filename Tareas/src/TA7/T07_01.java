ce37a89769be07833f3abe393456e4534226041e
DjAdria001 <adria30042004@gmail.com>
2025-03-12 11:37:55 +0100

    Update T07_01.java

--- Tareas/src/TA7/T07_01.java
+++ Tareas/src/TA7/T07_01.java
@@ -1,19 +1,32 @@
 package TA7;
 
+import javax.swing.JOptionPane;
 import java.util.ArrayList;
 
 public class T07_01 {
 	public static void main(String[] args) {
-		ArrayList<String> lista=new ArrayList<>();
-		lista.add("a");
-		lista.add("b");
-		lista.add("c");
-		lista.add("d");
-		lista.add("e");
-//		lista.size();
-		for(Object o:lista) {
-			System.out.print(o+" ");
-			
-		}System.out.println(lista.size());
+		ArrayList<Integer> Notas = new ArrayList<>();
+		String Pregunta = JOptionPane.showInputDialog("Cuantos alumnos hay?");
+		int PregNum = Integer.parseInt(Pregunta);
+		int i = 0;
+		int j = 1;
+
+		while (i < PregNum) {
+			int PregNum2;
+			do {
+				String Pregunta2 = JOptionPane.showInputDialog("Cual es la nota del alumno " + (i + 1));
+				PregNum2 = Integer.parseInt(Pregunta2);
+			} while (PregNum2 > 10 || PregNum2 < 0);
+			Notas.add(PregNum2);
+
+			i++;
+
+		}
+		
+		for (Object o : Notas) {
+			System.out.println("La nota del alumno " + j + " es " + o);
+			j++;
+		}
 	}
+
 }
