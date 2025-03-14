c2982ffc0670a921836f32728fe2c882076c83bf
DjAdria001 <adria30042004@gmail.com>
2025-03-14 12:16:43 +0100

    Update Testing.java

--- Testeo/src/Testing.java
+++ Testeo/src/Testing.java
@@ -1,12 +1,13 @@
 import java.util.Random;
-
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
 import javax.swing.JOptionPane;
 
 public class Testing {
 
 	public static void main(String[] args) {
-		int a=5, b=6;
-	System.out.printf("Numero %d y %d", a , b);//%d es una variable y %n es salto de linea
+		
 	}
 
 }
@@ -14,4 +15,6 @@ public class Testing {
 //		String Pregunta = JOptionPane.showInputDialog(" ");
 //		int PregNum = Integer.parseInt(Pregunta);
 //		javax.swing.JOptionPane.showMessageDialog(null,"");
+//int a=5, b=6;
+//System.out.printf("Numero %d y %d", a , b);//%d es una variable y %n es salto de linea
 
