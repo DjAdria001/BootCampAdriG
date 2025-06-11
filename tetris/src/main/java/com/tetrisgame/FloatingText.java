136816c2e9e1551c259dabe7062a701cfd457b93
DjAdria001 <adria30042004@gmail.com>
2025-06-11 12:35:53 +0200

    1

--- tetris/src/main/java/com/tetrisgame/FloatingText.java
+++ tetris/src/main/java/com/tetrisgame/FloatingText.java
@@ -17,8 +17,8 @@ public class FloatingText {
         this.y = y;
     }
     public boolean update() {
-        y -= 2;
-        alpha -= 15;
+        y -= 4;
+        alpha -= 30;
         return alpha > 0;
     }
 
